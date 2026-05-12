855ae8fdd59a8eda0326a24c5ba7fc68d50b770b
Jorge Lopez Vargas <jorgaf@gmail.com>
2026-05-12 15:42:58 -0500

    version secuencial

---
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/s6/App.java
@@ -0,0 +1,10 @@
+package ec.edu.utpl.carreras.computacion.proava.s6;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
