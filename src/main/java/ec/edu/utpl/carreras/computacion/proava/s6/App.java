86af70959afcf7d8827f0f3a72fe7ef75452493f
Jorge Lopez Vargas <jorgaf@gmail.com>
2026-05-12 17:33:46 -0500

    Versión secuencial

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/s6/App.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/s6/App.java
@@ -1,10 +1,19 @@
 package ec.edu.utpl.carreras.computacion.proava.s6;
 
+import ec.edu.utpl.carreras.computacion.proava.s6.util.URLExpanderTask;
+import java.util.Optional;
+
 /**
  * Hello world!
  */
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        URLExpanderTask urlExpanderTask = new URLExpanderTask();
+        Optional<String> expandedUrl = urlExpanderTask.expand("https://t.co/TGqER33gn7");
+        expandedUrl.ifPresentOrElse(
+                System.out::println,
+                () -> System.out.println("No se puede leer la URL")
+        );
     }
 }
---
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/s6/util/URLExpanderTask.java
@@ -0,0 +1,37 @@
+package ec.edu.utpl.carreras.computacion.proava.s6.util;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Optional;
+
+public class URLExpanderTask {
+
+    private static final HttpClient CLIENT = HttpClient.newBuilder()
+        .connectTimeout(Duration.ofSeconds(5))
+        .followRedirects(HttpClient.Redirect.ALWAYS)
+        .build();
+
+    public Optional<String> expand(String urlShortened) {
+        try {
+            var request = HttpRequest.newBuilder()
+                .uri(URI.create(urlShortened))
+                .timeout(Duration.ofSeconds(5))
+                .method("HEAD", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+            HttpResponse<Void> response = CLIENT.send(
+                request,
+                HttpResponse.BodyHandlers.discarding()
+            );
+            return switch (response.statusCode()) {
+                case 200 -> Optional.of(response.uri().toString());
+                default -> Optional.empty();
+            };
+        } catch (Exception _) {
+            return Optional.empty();
+        }
+    }
+}
